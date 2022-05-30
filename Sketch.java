c4133f5d60fd55a8aaf7739117f22ed8eea18145
jiayetguo <tylerguo23@ycdsbk12.ca>
2022-05-30 11:07:02 -0400

    SHEEEEEESH the movement works and the camera stays cenetered on the player while moving

--- Sketch.java
+++ Sketch.java
@@ -1,14 +1,24 @@
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Sketch extends PApplet {
-	
-	
+  PImage background1;
+  PImage playerJoe;
+
+  int playerX;
+  int playerY;
+  int playerSpd = 5;
+	boolean upPressed;
+  boolean downPressed;
+  boolean leftPressed;
+  boolean rightPressed;
+
   /**
    * Called once at the beginning of execution, put your size all in this method
    */
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(800, 800);
   }
 
   /** 
@@ -17,20 +27,64 @@ public class Sketch extends PApplet {
    */
   public void setup() {
     background(210, 255, 173);
+    background1 = loadImage("background-game.png");
+    playerJoe = loadImage("Gardevoir_Up.png");
+    playerX = 400;
+    playerY = 400;
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
-
-    stroke(255);
-    line(50, 125, 70, 50);  
+    if(upPressed){
+      playerY -= playerSpd;
+    }
+    if(downPressed){
+      playerY += playerSpd;
+    }
+    if(leftPressed){
+      playerX -= playerSpd;
+    }
+    if(rightPressed){
+      playerX += playerSpd;
+    }
+    translate(-playerX+400, -playerY+400);
+	  image(background1, 0, 0);
+    image(playerJoe, 100, 100);
+    
+    image(playerJoe, playerX, playerY);
   }
-  
-  // define other methods down here.
+
+  // Set booleans when wasd keys are pressed
+  public void keyPressed() {
+    if (key == 'w') {
+      upPressed = true;
+    }
+    else if (key == 's') {
+      downPressed = true;
+    }
+    else if (key == 'a') {
+      leftPressed = true;
+    }
+    else if (key == 'd') {
+      rightPressed = true;
+    }
+  }
+
+  // Set booleans when wasd keys are released
+  public void keyReleased() {
+    if (key == 'w') {
+      upPressed = false;
+    }
+    else if (key == 's') {
+      downPressed = false;
+    }
+    else if (key == 'a') {
+      leftPressed = false;
+    }
+    else if (key == 'd') {
+      rightPressed = false;
+    }
+  }
 }
