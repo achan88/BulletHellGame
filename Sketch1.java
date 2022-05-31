7a33a2691772dc896983b32674b37bc3d50d3be6
Andrew <andrew.chan23@ycdsbk12.ca>
2022-05-30 22:35:06 -0400

    moves sprite different directions

--- Sketch1.java
+++ Sketch1.java
@@ -1,14 +1,26 @@
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Sketch1 extends PApplet {
-	
-	
+  PImage background1;
+  PImage playerJoe;
+
+  int playerX;
+  int playerY;
+  int playerSpd = 5;
+	boolean upPressed;
+  boolean downPressed;
+  boolean leftPressed;
+  boolean rightPressed;
+
+  PImage [] player = new PImage[8];
+
   /**
    * Called once at the beginning of execution, put your size all in this method
    */
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(800, 800);
   }
 
   /** 
@@ -17,20 +29,88 @@ public class Sketch1 extends PApplet {
    */
   public void setup() {
     background(210, 255, 173);
+    background1 = loadImage("normal_background.png");
+    player[0] = loadImage("Gardevoir_Up.png");
+    player[1] = loadImage("Gardevoir_Down.png");
+    player[2] = loadImage("Gardevoir_Right.png");
+    player[3] = loadImage("Gardevoir_Left.png");
+    player[4] = loadImage("Gardevoir_Up_Left.png");
+    player[5] = loadImage("Gardevoir_Up_Right.png");
+    player[6] = loadImage("Gardevoir_Down_Left.png");
+    player[7] = loadImage("Gardevoir_Down_Right.png");
+    playerX = 400;
+    playerY = 400;
+    playerJoe = player[1];
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    if(upPressed){
+      playerY -= playerSpd;
+      playerJoe = player[0];
+    }
+    if(downPressed){
+      playerY += playerSpd;
+      playerJoe = player[1];
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    }
+    if(leftPressed){
+      playerX -= playerSpd;
+      playerJoe = player[3];
+    }
+    if(rightPressed){
+      playerX += playerSpd;
+      playerJoe = player[2];
+    }
+    if (upPressed && leftPressed) {
+      playerJoe = player[4];
+    }
+    if (upPressed && rightPressed) {
+      playerJoe = player[5];
+    }
+    if (downPressed && leftPressed) {
+      playerJoe = player[6];
+    }
+    if (downPressed && rightPressed) {
+      playerJoe = player[7];
+    }
+    translate(-playerX+400, -playerY+400);
+	  image(background1, 0, 0);
+    
+    image(playerJoe, playerX, playerY);
   }
-  
-  // define other methods down here.
+
+  // Set booleans when wasd keys are pressed
+  public void keyPressed() {
+    if (key == 'w') {
+      upPressed = true;
+    }
+    else if (key == 's') {
+      downPressed = true;
+    }
+    else if (key == 'a') {
+      leftPressed = true;
+    }
+    else if (key == 'd') {
+      rightPressed = true;
+    }
+  }
+
+  // Set booleans when wasd keys are released
+  public void keyReleased() {
+    if (key == 'w') {
+      upPressed = false;
+    }
+    else if (key == 's') {
+      downPressed = false;
+    }
+    else if (key == 'a') {
+      leftPressed = false;
+    }
+    else if (key == 'd') {
+      rightPressed = false;
+    }
+  }
 }
