feb1773086f655e9bdfc22734f381b4ac3b40699
jiayetguo <tylerguo23@ycdsbk12.ca>
2022-06-06 11:15:47 -0400

    Added enemy bullet collision with player Added playerHealth and healthbar Added rectangle x rectangle collision method Switched sprites and hitboxes to be centered on coordinates Fixed bug that caused bullets that hit the player and left the map on the same frame to be deleted twice and crash the game

--- Sketch2.java
+++ Sketch2.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 import java.util.Iterator;
 
+import javax.swing.text.DefaultStyledDocument.ElementSpec;
+
 import processing.core.PApplet;
 import processing.core.PImage;
 
@@ -16,18 +18,23 @@ public class Sketch2 extends PApplet {
   int bossSpd = 5;
   boolean bossAlive = true;
   int bossHealth = 500;
+  int phase = 1;
 
-  int playerRectX;
-  int playerRectY;
-  int playerSpd = 5;
+  int playerX;
+  int playerY;
+  int playerSpd = 10;
+  int playerHealth = 1000;
+  int combatTimer = 0;
 	boolean upPressed;
   boolean downPressed;
   boolean leftPressed;
   boolean rightPressed;
-  int phase = 1;
+  int bulletRed = color(255, 50, 50);
+
   
 
-  ArrayList <playerBullet> bullets = new ArrayList <playerBullet>();
+  ArrayList <playerBullet> playerBullets = new ArrayList <playerBullet>();
+  ArrayList <normalBullet> normalBullet = new ArrayList <normalBullet>();
   ArrayList <rectBullet> rectBullets = new ArrayList <rectBullet>();
 
   PImage [] player = new PImage[8];
@@ -60,8 +67,8 @@ public class Sketch2 extends PApplet {
     player[5] = loadImage("Gardevoir_Up_Right.png");
     player[6] = loadImage("Gardevoir_Down_Left.png");
     player[7] = loadImage("Gardevoir_Down_Right.png");
-    playerRectX = 400;
-    playerRectY = 400;
+    playerX = 800;
+    playerY = 800;
     playerSprite = player[1];
   }
 
@@ -69,89 +76,152 @@ public class Sketch2 extends PApplet {
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-
-    if (bossAlive) {
-
-      if(upPressed){
-        playerRectY -= playerSpd;
-        playerSprite = player[0];
-      }
-      if(downPressed){
-        playerRectY += playerSpd;
-        playerSprite = player[1];
   
-      }
-      if(leftPressed){
-        playerRectX -= playerSpd;
-        playerSprite = player[3];
-      }
-      if(rightPressed){
-        playerRectX += playerSpd;
-        playerSprite = player[2];
-      }
-      if (upPressed && leftPressed) {
-        playerSprite = player[4];
-      }
-      else if (upPressed && rightPressed) {
-        playerSprite = player[5];
-      }
-      else if (downPressed && leftPressed) {
-        playerSprite = player[6];
-      }
-      else if (downPressed && rightPressed) {
-        playerSprite = player[7];
-      }
+    if(combatTimer>0){
+    combatTimer--;
+    }
+    if(combatTimer == 0 && playerHealth < 1000 && frameCount % 2 == 0){
+      playerHealth++;
+    }
+    if(upPressed && playerY > 400){
+      playerY -= playerSpd;
+      playerSprite = player[0];
+    }
+    if(downPressed && playerY < 1586){
+      playerY += playerSpd;
+      playerSprite = player[1];
+    }
+    if(leftPressed && playerX > 412){
+      playerX -= playerSpd;
+      playerSprite = player[3];
+    }
+    if(rightPressed && playerX < 1588){
+      playerX += playerSpd;
+      playerSprite = player[2];
+    }
+    if (upPressed && leftPressed) {
+      playerSprite = player[4];
+    }
+    else if (upPressed && rightPressed) {
+      playerSprite = player[5];
+    }
+    else if (downPressed && leftPressed) {
+      playerSprite = player[6];
+    }
+    else if (downPressed && rightPressed) {
+      playerSprite = player[7];
+     }
   
-      translate(-playerRectX+400, -playerRectY+400);
+    translate(-playerX+400, -playerY+400);
   
-      if(phase == 1){
+    if(phase == 1){
       image(background1, 0, 0);
-      }
-      if(phase == 2){
+    }
+    if(phase == 2){
       image(background2, 0, 0);
-      }
-      if(phase == 3){
+    }
+     if(phase == 3){
       image(background3, 0, 0);
-      }
-      image(playerSprite, playerRectX, playerRectY);
+    }
+    image(playerSprite, playerX-12, playerY-14);
   
-      image(bossSprite, bossX, bossY);
+
+
+    /**
+     * 
+     * LOAD BULLETS
+     * 
+     */
+
+    // player bullets
+    if (frameCount%5==0 && mousePressed) {
+      playerBullet b = new playerBullet(playerX, playerY, mouseX+playerX-400, mouseY+playerY-400);
+      playerBullets.add(b);
+    }
+     Iterator <playerBullet> playerItr = playerBullets.iterator();
+    while(playerItr.hasNext()) {
+      playerBullet i = playerItr.next();
+      i.update();
+      boolean collide = circleRect(i.X, i.Y, 4f, bossX-42, bossY-50, 80,  80);
+      if (collide) {
+        bossHealth -= 5;
+        playerItr.remove();
+      }
+      else if (i.X > 1600 || i.X < 400 || i.Y > 1600 || i.Y < 400){
+        playerItr.remove();
+      }
+    }
+
+    // rectangle bullets
+    if (frameCount%30==0) {
+      rectBullet b = new rectBullet(bossX, bossY, 0, 10, 80, 5, 60, 1, 0, 20);
+      rectBullets.add(b);
+     }
+    Iterator <rectBullet> rectItr = rectBullets.iterator();
+    while(rectItr.hasNext()){
+      rectBullet i = rectItr.next();
+      i.update();
+      if(rectRect(i.X, i.Y, (float) i.width, (float) i.height, playerX-12, playerY-14, 23, 28)){
+        playerHealth -= i.damage ;
+        rectItr.remove();
+        combatTimer = 90;
+      }
+      else if (i.X > 1600 || i.X < 400 || i.Y > 1600 || i.Y < 400 || i.time == i.duration || i.width < 0 || i.height < 0){
+      rectItr.remove();
+      }
+    }
+
+    // normal bullets
+    // if (frameCount%5 == 0) {
+    //   normalBullet b = new normalBullet(bossX, bossY, playerX, playerY, false, 8);
+    //   normalBullet.add(b);
+    // }
+
+    Iterator <normalBullet> normalItr = normalBullet.iterator();
+    while(normalItr.hasNext()) {
+      normalBullet i = normalItr.next();
+      i.update();
+      if (circleRect(i.X, i.Y, i.size, playerX-12, playerY-14, 23,  28)) {
+        playerHealth -= 5;
+        normalItr.remove();
+        combatTimer = 90;
+      }
+      else if (i.X > 1600 || i.X < 400 || i.Y > 1600 || i.Y < 400){
+        normalItr.remove();
+      }
+    }
+
+
+
+    /**
+     * 
+     * BOSS ATTACKS
+     * 
+     */
+
+
+
+
+
+
+    
+
+
+     /**
+      * 
+      * DRAW MAJOR SPRITES AND GRAPHICS 
+      *
+      */
+    if (bossAlive) {
+      image(bossSprite, bossX-42, bossY-50);
       bossX += bossSpd;
       if (bossX <= 500 || bossX >= 1416) {
         bossSpd = -bossSpd;
       }
-  
-      if (frameCount%5==0 && mousePressed) {
-       playerBullet b = new playerBullet(playerRectX+12, playerRectY+14, mouseX+playerRectX-400, mouseY+playerRectY-400);
-       bullets.add(b);
-      }
-      Iterator <playerBullet> playerItr = bullets.iterator();
-      while(playerItr.hasNext()){
-        playerBullet i = playerItr.next();
-        i.update();
-        if (i.X > 1600 || i.X < 400 || i.Y > 1600 || i.Y < 400){
-        playerItr.remove();
-        }
-        boolean collide = circleRect(i.X, i.Y, 2.5f, bossX, bossY, 84,  100);
-        if (collide) {
-          bossHealth -= 5;
-          playerItr.remove();
-        }
-      }
-      if (frameCount%30==0) {
-        rectBullet b = new rectBullet(bossX, bossY, 0, 10, 80, 5, 60, 1, 0);
-        rectBullets.add(b);
-       }
-      Iterator <rectBullet> rectItr = rectBullets.iterator();
-      while(rectItr.hasNext()){
-        rectBullet i = rectItr.next();
-        i.update();
-        if (i.X > 1600 || i.X < 400 || i.Y > 1600 || i.Y < 400 || i.time == i.duration || i.width < 0 || i.height < 0){
-        rectItr.remove();
-        }
-      }
+    }
 
-      translate(playerRectX-400, playerRectY-400);
+    translate(playerX-400, playerY-400);
+    if (bossHealth >= 1) {
       fill(50);
       rect(400, 50, 250, 10);
       fill(255, 0, 0);
@@ -159,13 +229,24 @@ public class Sketch2 extends PApplet {
       rect(400, 50, bossHealth/2, 10);
       fill(255);
       text(bossHealth, 600, 60);
+      } 
+
+      fill (50);
+      rect(20, 700, 100, 5);
+      fill(0, 255, 0);
+      stroke(0);
+      rect(20, 700, playerHealth/10, 10);
+      fill(255);
+      text(playerHealth, 80, 700);
+
+    if (bossHealth <= 0) {
+      bossAlive = false;
     }
+    
+    
     // System.out.println(millis());
 
   }
-
-
-
 
   // Set booleans when wasd keys are pressed
   public void keyPressed() {
@@ -199,6 +280,7 @@ public class Sketch2 extends PApplet {
     }
   }
 
+  // calculates collision between circle and rectangle
   public boolean circleRect(float circleX, float circleY, float radius, float rectangleX, float rectangleY, float rectangleWidth, float rectangleHeight) {
     float tempX = circleX;
     float tempY = circleY;
@@ -226,6 +308,13 @@ public class Sketch2 extends PApplet {
     return false;
   }
 
+  public boolean rectRect(float rect1X, float rect1Y, float rect1Width, float rect1Height, float rect2X, float rect2Y, float rect2Width, float rect2Height){
+    if (rect1X < rect2X + rect2Width && rect1X+rect1Width > rect2X && rect1Y < rect2Y + rect2Height && rect1Y + rect1Height > rect2Y){
+      return true;
+    } 
+    return false;
+  }
+
   class playerBullet {
   int X;
   int Y;
@@ -246,8 +335,8 @@ public class Sketch2 extends PApplet {
   dx /= length;
   dy /= length;
 
-  velX = dx * 10;
-  velY = dy * 10;
+  velX = dx * 20;
+  velY = dy * 20;
   }
 
   void update(){
@@ -256,11 +345,54 @@ public class Sketch2 extends PApplet {
     fill(255, 160, 255);
     strokeWeight(1);
     stroke(75, 0, 130);
-    ellipse(X, Y, 5, 5);
-  } 
-
+    ellipse(X, Y, 8, 8);
+   }
   }
 
+  class normalBullet {
+    int X;
+    int Y;
+    double velX;
+    double velY;
+    double dx;
+    double dy;
+    double length;
+    boolean isGold;
+    int size; 
+  
+    normalBullet(int x, int y, int destx, int desty, boolean isGold, int size) {
+      this.X = x;
+      this.Y = y;
+      dx = destx - X;
+      dy = desty - Y;
+
+      this.isGold = isGold;
+      this.size = size;
+
+      length = Math.sqrt(dx*dx + dy*dy);
+  
+      dx /= length;
+      dy /= length;
+  
+      velX = dx * 20;
+      velY = dy * 20;
+    }
+  
+    void update() {
+      X += velX;
+      Y += velY;
+      if (isGold) {
+        fill(243, 236, 214);
+      }
+      else {
+        fill(bulletRed);
+      }
+      strokeWeight(1);
+      stroke(0);
+      ellipse(X, Y, size, size);
+    }
+
+  }
   class rectBullet {
     int X;
     int Y;
@@ -272,8 +404,9 @@ public class Sketch2 extends PApplet {
     double height;
     double shrinkX;
     double shrinkY;
-  
-    rectBullet(int x, int y, double velX, double velY, double width, double height, int duration, double shrinkX, double shrinkY){
+    int damage;
+
+    rectBullet(int x, int y, double velX, double velY, double width, double height, int duration, double shrinkX, double shrinkY, int damage){
     this.X = x;
     this.Y = y;
     this.velX = velX;
@@ -283,9 +416,10 @@ public class Sketch2 extends PApplet {
     this.duration = duration;
     this.shrinkX = shrinkX;
     this.shrinkY = shrinkY;
+    this.damage = damage;
     time = 0;
     }
-  
+
     void update(){
       time++;
       X += velX;
@@ -296,7 +430,7 @@ public class Sketch2 extends PApplet {
       strokeWeight(1);
       stroke(0);
       rect (X, Y, (float) width, (float) height);
-    }  
     }
+  }
 
 }
